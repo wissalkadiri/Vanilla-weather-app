6f853e14fc6f358b12cfc5a30d1985420890f883
wissalkadiri <79605648+wissalkadiri@users.noreply.github.com>
2022-10-10 14:57:31 +0100

    Forecast / API

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ function formatDate(timestamp){
     let day =days[date.getDay()]
     return `${day} ${hours}:${minutes}`
 }
-function displayForecast() {
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
   
     let days = ["Mon", "Thu", "Wed", "Thu", "Fri"];
@@ -41,7 +41,11 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
     console.log(forecastHTML);
   }
-
+ function getForecast(coordinates){
+    let apiKey= "1b60c04b0a29fc7192025fa7341885b7";
+    let apiUrl =`https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayForecast)
+}
 
 function displayTemperature(response){
    let temperatureElement = document.querySelector("#temperature");
@@ -66,6 +70,8 @@ function displayTemperature(response){
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
    )
    iconElement.setAttribute("alt",response.data.weather[0].description)
+
+   getForecast(response.data.coord);
 }
 function search(city){
     let apiKey= "1b60c04b0a29fc7192025fa7341885b7";
