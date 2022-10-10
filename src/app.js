0ef2837d8a94cde132b3bfd382e077ce03f3742c
wissalkadiri <79605648+wissalkadiri@users.noreply.github.com>
2022-10-10 14:33:48 +0100

    Adding Html on JS

--- src/app.js
+++ src/app.js
@@ -12,7 +12,35 @@ function formatDate(timestamp){
     let day =days[date.getDay()]
     return `${day} ${hours}:${minutes}`
 }
-
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+  
+    let days = ["Mon", "Thu", "Wed", "Thu", "Fri"];
+  
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/04d@2x.png"
+            alt=""
+            width="86"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+    `;
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
+  }
 
 
 function displayTemperature(response){
@@ -23,6 +51,9 @@ function displayTemperature(response){
    let windElement =document.querySelector("#wind")
    let dateElement=document.querySelector("#date")
    let iconElement =document.querySelector("#icon")
+    
+
+
    celsiusTemperature=response.data.main.temp
    temperatureElement.innerHTML=Math.round(response.data.main.temp)
    cityElement.innerHTML=response.data.name
@@ -65,6 +96,7 @@ function celsiusLink(event){
 
 
 let celsiusTemperature =null;
+displayForecast();
 
 let form =document.querySelector("#searchBtn");
 form.addEventListener("click",handleSubmit);
